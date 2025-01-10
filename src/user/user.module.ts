50ceff8f327e6cc5c6ce5a76169385a591604e39
elina-chertova <elas.2015@mail.ru>
2025-01-10 15:55:54 +0300

    - tweets table

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -5,12 +5,13 @@ import { UserService } from './user.service';
 import { Trade } from './trade.entity';
 import { ActivityEntity } from './activity.entity';
 import { Queue } from './queue.entity';
+import { Tweet } from './tweet.entity';
 import { InterfaceSocialService } from '../interface-social/interface-social.service';
 import { HttpModule } from '@nestjs/axios';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([User, Trade, ActivityEntity, Queue]),
+    TypeOrmModule.forFeature([User, Trade, ActivityEntity, Queue, Tweet]),
     HttpModule,
   ],
   providers: [UserService, InterfaceSocialService],
