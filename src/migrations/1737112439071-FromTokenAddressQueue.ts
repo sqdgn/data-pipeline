57e360ad4869707a908bb4d604fb2c0fa9acd2a8
elina-chertova <elas.2015@mail.ru>
2025-01-17 14:50:14 +0300

    - context to queue

--- src/migrations/1737112439071-FromTokenAddressQueue.ts
+++ src/migrations/1737112439071-FromTokenAddressQueue.ts
@@ -5,11 +5,9 @@ export class FromTokenAddressQueue1737112439071 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "queue" ADD "fromTokenAddress" character varying`);
-        await queryRunner.query(`ALTER TABLE "queue" ADD "context" json`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "queue" DROP COLUMN "context"`);
         await queryRunner.query(`ALTER TABLE "queue" DROP COLUMN "fromTokenAddress"`);
     }
 
