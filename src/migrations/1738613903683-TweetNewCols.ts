dccc6b670d13e9e1bf25ca023388260dee7a9dc0
elina-chertova <elas.2015@mail.ru>
2025-02-04 11:59:23 +0300

    - tweets

--- src/migrations/1738613903683-TweetNewCols.ts
+++ src/migrations/1738613903683-TweetNewCols.ts
@@ -4,9 +4,9 @@ export class TweetNewCols1738613903683 implements MigrationInterface {
     name = 'TweetNewCols1738613903683'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "tweets" ADD "activityId" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "tweets" ADD "activityId" character varying`);
         await queryRunner.query(`ALTER TABLE "tweets" ADD "retweeted" boolean NOT NULL DEFAULT false`);
-        await queryRunner.query(`ALTER TABLE "tweets" ADD "tweetId" character varying NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "tweets" ADD "tweetId" character varying`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
